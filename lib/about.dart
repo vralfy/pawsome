5723f984142e5f5f69aa69c2de439af4285ad159
Norman Specht <norman.specht.extern@atruvia.de>
2023-05-31 18:44:43 +0200

    get available directories to store config files

--- lib/about.dart
+++ lib/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:p6_ui/container/scrollcontainer.dart';
 import 'package:p6_ui/headline/headline.dart';
 import 'package:p6_ui/view.dart';
 import 'package:p6_ui/widget.dart';
@@ -9,39 +10,50 @@ class About extends P6StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return View.centered(
-        context,
-        Table(
+      context,
+      ScrollContainer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const TableRow(children: [
-              Headline1(text: 'In progress'),
-              Headline1(text: 'In progress'),
-            ]),
-            TableRow(children: [
-              Text("Config directories:"),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      const Text('Documents: '),
-                      Text(configuration.directory_documents?.path ?? 'unknown'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Text('Temp: '),
-                      Text(configuration.directory_temporary?.path ?? 'unknown'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Text('Download: '),
-                      Text(configuration.directory_download?.path ?? 'unknown'),
-                    ],
-                  ),
-                ],
-              )
-            ])
+            const Headline1(text: 'System'),
+            Table(
+              children: [
+                TableRow(children: [
+                  const Text("Config directories:"),
+                  const Text('Temp: '),
+                  Text(configuration.directory_temporary?.path ?? 'unknown'),
+                ]),
+                TableRow(children: [
+                  const Text(""),
+                  const Text('External: '),
+                  Text(configuration.directory_external?.path ?? 'unknown'),
+                ]),
+                TableRow(children: [
+                  const Text(""),
+                  const Text('Libs: '),
+                  Text(configuration.directory_library?.path ?? 'unknown'),
+                ]),
+                TableRow(children: [
+                  const Text(""),
+                  const Text('Support: '),
+                  Text(configuration.directory_support?.path ?? 'unknown'),
+                ]),
+                TableRow(children: [
+                  const Text(""),
+                  const Text('Documents: '),
+                  Text(configuration.directory_documents?.path ?? 'unknown'),
+                ]),
+                TableRow(children: [
+                  const Text(""),
+                  const Text('Download: '),
+                  Text(configuration.directory_download?.path ?? 'unknown'),
+                ]),
+              ],
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
